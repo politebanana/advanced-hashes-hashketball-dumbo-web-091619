202a4605db5a27d93dbe852efa0ff7d6afec49bc
Christian Zambrana <chrislajas@gmail.com>
2019-08-01 01:32:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -243,8 +243,6 @@ def player_with_longest_name
   player_with_most_of(:player_name)
 end
 
-# # Super Bonus Question
-
 def long_name_steals_a_ton?
   player_with_most_of(:steals) == player_with_most_of(:player_name)
 end
